677e17db5d211fd7b0331b90718f4e235a1ab289
m.sosnov <crazymax101@gmail.com>
2026-05-10 08:46:03 +0700

    fix: baseUrl = /jira-helper/ for GitHub Pages

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://jira-helper.github.io',
-  baseUrl: '/',
+  baseUrl: '/jira-helper/',
 
   organizationName: 'jira-helper',
   projectName: 'jira-helper',
